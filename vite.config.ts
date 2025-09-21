af38eccfdddedf501e1d3274f9643f4b48e03930
Pablo <89890109+PabloGomez-97@users.noreply.github.com>
2025-09-21 18:00:38 -0300

    update1

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-    assetsDir: 'assets'
-  },
-  server: {
-    port: 3000
-  },
-  base: './'
+    outDir: 'dist'
+  }
 })
